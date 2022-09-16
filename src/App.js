f29fdcf031053b02a6ab45125720978b22a62965
Udo-Eng <udochukwuabazie.soft@gmail.com>
2022-09-16 09:44:58 +0100

    Added advanced Redux functionality to the cart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
+import {useEffect,Fragment}from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import {useSelector} from 'react-redux';
+import {useSelector,useDispatch} from 'react-redux';
+import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
 
+
+
+let isInitial = true ;
 
 function App() {
-
+// useSelector   set up a subcription to the store 
   const showCart = useSelector((state) => state.ui.isCartVisible);
+  const  cart = useSelector((state) => state.cart);
+  const  notification = useSelector((state) => state.ui.notification)
+  const  dispatch = useDispatch();
+
+
+
+  useEffect(()=>{
+     dispatch(fetchCartData());
+  },[dispatch])
+
+  //Function setup to perform asynchronous task
+  useEffect(() =>{
+
+   if(isInitial){
+     isInitial = false ;
+     return;
+   }
+   if(cart.changed){
+     dispatch(sendCartData(cart));
+   }
+     
+  },[cart,dispatch]);
+
+
 
   return (
-    <Layout>
-      { showCart && <Cart /> }
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+
 const cartSlice = createSlice({
   name: "cart",
   initialState: { 
       items: [],
-      totalQuantity: 0
+      totalQuantity: 0,
+      changed: false
   },
   reducers: {
+    replaceCart(state,action){
+        state.items = action.payload.items;
+        state.totalQuantity = action.payload.totalQuantity;
+    },
     addCartItem(state,action) {
        let newItem = action.payload;
        let existingItem = state.items.find(item => item.id === newItem.id);
        state.totalQuantity++;
+       state.changed = true;
        if (!existingItem) {
          state.items.push({
             id : newItem.id,
@@ -27,17 +34,20 @@ const cartSlice = createSlice({
     removeCartItem(state,action){
          let id = action.payload;
          state.totalQuantity--;
+         state.changed = true;
          let existingItem = state.items.find((item) => item.id === id );
 
          if(existingItem.quantity === 1){
              state.items = state.items.filter(item => item.id !== id);
          }else{
-                     existingItem.quantity -= 1;
-                     existingItem.totalprice -= existingItem.price;  
+              existingItem.quantity -= 1;
+              existingItem.totalprice -= existingItem.price;  
          }
     }
   },
 });
+
+
 
 export const cartActions = cartSlice.actions;
 
